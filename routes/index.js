d940893c3a4a2c901e5263e7f5dd7cc9e39d426c
James Chow <zhouxy.vortex@gmail.com>
2018-05-11 10:59:37 +0800

    query/show results in index page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 var express = require('express');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '123456',
+  database: 'googlescrape'
+});
 var router = express.Router();
+connection.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var datas = [{"index": 0, "Name": "James", "Website": "https://www.baidu.com"}, {"index": 1, "Name": "Daniel", "Website": "https://www.google.com"}];
-  res.render('index', { datas: datas });
+  connection.query('select r.domain, m.value, z.name from (select domain, keywords from results group by domain, keywords) r, keywords m, search_engine z where z.id = m.search_engine and r.keywords = m.id', [], function (e, r) {
+    if (e) return;
+    res.render('index', { datas: r });
+  });
+
 });
 
 module.exports = router;
